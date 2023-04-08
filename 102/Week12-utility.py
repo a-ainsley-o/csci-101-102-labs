a3cbe47f4cd11deaa3e7fde2e3a04647b7d11875
Addison Olstad <addisonolstad@mines.edu>
2023-04-07 19:02:54 -0600

    update_string added

--- 102/Week12-utility.py
+++ 102/Week12-utility.py
@@ -7,4 +7,11 @@
 def load_file(filename):
     lines = filename.splitlines()
     print("OUTPUT", lines)
-    
+
+def update_string(string1, string2, integer):
+    str_1_lines = list(string1)
+    str_1_lines[integer] = string2
+    empty_str = ""
+    for i in str_1_lines:
+        empty_str += i
+    print("OUTPUT", empty_str)
